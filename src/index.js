bc606b572df74d3eb4408fbf7ffd3c3fc6860fbd
Alex <asgaroth@yandex.ru>
2023-01-15 11:28:23 +0300

    feat: add resolve

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    const nStr = String(n);
+    let response = '';
+
+    for (let i = nStr.length - 1; i >= 0; i--) {
+        if (nStr[i] === '-') {
+            continue;
+        }
+        response += nStr[i];
+    }
+    
+    return Number(response);
 }
